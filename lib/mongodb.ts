36fc880f3556c2df5093d141e1397a9fb61011c3
Enrique Caballero <ac.enriquealonso@gmail.com>
2026-05-31 15:42:34 +0200

    toqueteo en lib/mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,22 +1,24 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string;
-const options = {};
-
 let clientPromise: Promise<MongoClient>;
+
+function getClientPromise() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) throw new Error("MONGODB_URI no definida");
+  const client = new MongoClient(uri);
+  return client.connect();
+}
 
 if (process.env.NODE_ENV === "development") {
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
   if (!globalWithMongo._mongoClientPromise) {
-    const client = new MongoClient(uri, options);
-    globalWithMongo._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = getClientPromise();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
-  const client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  clientPromise = getClientPromise();
 }
 
 export default clientPromise;
