20c4536f3f618d3f50d883e4d05c5b8d194ed704
Enrique Caballero <ac.enriquealonso@gmail.com>
2026-05-31 13:38:49 +0200

    otra prueba

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,24 +3,19 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI as string;
 const options = {};
 
-let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!uri) throw new Error("MONGODB_URI no está definida en las variables de entorno");
-
 if (process.env.NODE_ENV === "development") {
-  // En desarrollo, reutiliza la conexión entre hot-reloads
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    const client = new MongoClient(uri, options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
-  // En producción, crea una conexión nueva
-  client = new MongoClient(uri, options);
+  const client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
